94ed187cf60ed8692adcc3784e38c53fce68ae64
StanleyKhashane <Stanleyoda70@gmail.com>
2022-08-22 14:03:21 +0000

    Print string

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-Holberton_school =str[0:9]#first 9 characters from index 0
-print("halberton school" * 3)
-print(Holberton_school)
+print(str * 3)
+print(str[:9])
