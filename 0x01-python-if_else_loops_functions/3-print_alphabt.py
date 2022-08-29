349b6d08a65bfdf80706c51aaf88f7d63057a1b9
StanleyKhashane <Stanleyoda70@gmail.com>
2022-08-29 10:41:09 +0000

    fixing pycodestyle

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-#this is version 3 of the code and it uses .replace() function to remove q and e 
-for ch in range(97,123):
-    print("{:c}".format(ch).replace("q","").replace("e",""), end='')
+for ch in range(97, 123):
+    print("{:c}".format(ch).replace("q", "").replace("e", ""), end='')
